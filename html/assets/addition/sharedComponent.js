d3f373401830fd71b5ee3113d270d1717c8e43cb
artemdymentartem <jhon.pastry123@gmail.com>
2021-08-09 16:03:15 +1000

    refactor login & register

--- html/assets/addition/sharedComponent.js
+++ html/assets/addition/sharedComponent.js
@@ -1,35 +1,4 @@
-const menu_data = [
-	{
-		id: "dashboard", icon: "mdi mdi-view-dashboard", value: "main", data: [
-			{ id: "dashboard1", value: "Dashboard " },
-		]
-	},
-	{
-		id: "layouts", icon: "mdi mdi-view-column", value: "analysis", data: [
-			{ id: "accordions", value: "an1" },
-			{ id: "portlets", value: "an2" }
-		]
-	},
-	{
-		id: "tables", icon: "mdi mdi-table", value: "managingFeed", data: [
-			{ id: "tables1", value: "manage1" },
-			{ id: "tables2", value: "manage2" },
-			{ id: "tables3", value: "manage3" }
-		]
-	},
-	{
-		id: "uis", icon: "mdi mdi-puzzle", value: "managingFarm", data: [
-			{ id: "dataview", value: "DataView" },
-			{ id: "list", value: "farm1" },
-			{ id: "menu", value: "farm2" },
-			{ id: "tree", value: "farm3" },
-			{ id: "register-farm", value: "register"},
-			{ id: "register-breed", value: "register breed"},
-			{ id: "register-worker", value: "register worker"}
-		]
-	},
 
-];
 
 webix.protoUI({
 	name:"photo",
